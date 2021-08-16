023868dfe29a4d70bdc8c6a3f2cb1aed8d85e0f7
debagnikroy <debagnikroy@gmail.com>
2021-08-16 23:54:12 +0530

    Update HashtagController.java

--- src/main/java/com/team6/SurveyService/controller/HashtagController.java
+++ src/main/java/com/team6/SurveyService/controller/HashtagController.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
