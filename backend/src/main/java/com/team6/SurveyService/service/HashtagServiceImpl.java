26341dc85af7166c95d7173ccc75540b000ac9b6
Purvansh Gourh <purvanshgaur26@gmail.com>
2021-08-16 15:51:13 +0530

    added backend

---
+++ backend/src/main/java/com/team6/SurveyService/service/HashtagServiceImpl.java
@@ -0,0 +1,44 @@
+package com.team6.SurveyService.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.team6.SurveyService.HashtagRepository.HashtagRepository;
+import com.team6.SurveyService.dto.Hashtags;
+
+@Service
+public class HashtagServiceImpl implements HashtagService {
+
+	
+	@Autowired
+	private HashtagRepository hashtagRepository;
+
+	@Override
+	public List<String> getTrendingHashtags() {
+		// TODO Auto-generated method stub
+		return hashtagRepository.getTrendingHashtag();
+	}
+}
+
+
+
+/*@Override
+	public Hashtags addHashtag(Hashtags hashtags) {
+		// TODO Auto-generated method stub
+		return hashtagRepository.save(hashtags);
+	}*/
+
+	/*@Override
+	public Hashtags getHashtag(String hashtag) {
+		// TODO Auto-generated method stub
+		return hashtagRepository.findById(hashtag).orElse(null);
+	}*/
+
+	/*@Override
+	public int getHashtagCount(String hashtag) {
+		// TODO Auto-generated method stub
+		return hashtagRepository.getHashtagCount(hashtag);
+	}*/
+
